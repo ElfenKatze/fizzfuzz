e4a4d5c50b120d55211db2c47663edcf2c8226fe
ElfenKatze <97521199+ElfenKatze@users.noreply.github.com>
2022-02-08 00:47:22 -0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 for fizz in range(0, 100):
-    if fizz% 3 == 0:
+    if fizz% 15 == 0:
+        print ("fizzbuzz")
+    elif fizz% 3 == 0:
         print ("fizz")
     elif fizz% 5 == 0:
         print ("buzz")
-    elif fizz% 15 == 0:
-        print ("fizzbuzz")
     else:
-        print (fizz, end=" ")
+        print (fizz)
